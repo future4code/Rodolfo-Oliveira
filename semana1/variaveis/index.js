13f4dc5afaca048f5aa99f33b5b800bff8565253
Rodolfo Dornelas <rodolfo.dpo@gmail.com>
2021-06-14 15:35:08 -0300

    Organizando os arquivos

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,72 @@
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+// Impressão no console:
+//10
+//10 5
+// let a = 10
+// let b = 20
+// let c
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+
+// Impressão no console:
+// 10 10 10
+//let p = prompt("Quantas horas você trabalha por dia?")
+// let t = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+// // Nomes melhores seriam nomes que definissem o que a variavel representa, por exemplo
+
+// let horasTrabalhada = prompt("Quantas horas você trabalha por dia?")
+// let valorHoraTrabalhada = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+// let nomeCompleto 
+// let idade 
+// console.log(typeof nomeCompleto, typeof idade)
+//o tipo é impresso como indefinido pois não foi atribuido valor a ele
+
+// let nomeCompleto = prompt ("Qual o seu nome?")
+// let idade = prompt ("qual a sua idade?")
+// console.log(typeof nomeCompleto, typeof idade)
+// O tipo foi alterado para string
+
+// let nomeCompleto = prompt ("Qual o seu nome?")
+// let idade = prompt ("qual a sua idade?")
+// console.log ('Ola', nomeCompleto, 'você tem', idade, 'anos.')
+
+
+// const respostaPrimeiraPergunta = prompt("Ja almoçou hoje?")
+// const respostaSegundaPergunta = prompt("você tem um coelho?")
+// const respostaTerceiraPergunta = prompt("Você ta com fome?")
+
+// console.log ("Você ja almoçou hoje?", respostaPrimeiraPergunta)
+// console.log ('Você tem um coelho?', respostaSegundaPergunta)
+// console.log ('Você ta com fome?', respostaTerceiraPergunta)
+
+// let a = 10
+// let b = 25
+// let c
+
+// c = a
+// a = b
+// b = c
+
+// console.log("O novo valor de a é", a)
+// console.log("O novo valor de b é", b)
+
+const numeroUm = prompt("Numero 1")
+const numeroDois = prompt("Numero 2")
+const numeroRealUm = Number(numeroUm)
+const numeroRealDois = Number(numeroDois)
+
+console.log ('O primeiro número somado ao segundo número resulta em:',numeroRealUm+numeroRealDois)git
+console.log ('O primeiro número multiplicado pelo segundo número resulta em:' ,numeroRealUm*numeroRealDois)
