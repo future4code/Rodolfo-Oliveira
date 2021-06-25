de9d355157fbe827c67078890290f5c5f04c5cbd
Rodolfo Dornelas <rodolfo.dpo@gmail.com>
2021-06-25 15:02:43 -0300

    To enviando porque preciso ir no médico, o desafio infelizmente fica pra depois

---
+++ semana2/blackjack/desafio.js
@@ -0,0 +1,59 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+   console.log ("Boas vindas ao jogo de Blackjack!")
+   if(confirm("Deseja iniciar uma nova Rodada?")) { 
+      const arrayUsuario = []
+      const arrayComputador = []
+      carta1 = comprarCarta()
+      carta2 = comprarCarta()
+      arrayUsuario.push(comprarCarta(), comprarCarta())
+      arrayComputador.push(comprarCarta(), comprarCarta())
+      const maoUsuario = arrayUsuario.map((item, index, array) => {
+         return item.texto
+      })
+      const valorUsuario = arrayUsuario.map((item, index, array) => {
+         return item.valor
+      })
+      const maoComputador = arrayComputador.map((item, index, array) =>{
+          return item.texto
+      })
+      const valorComputador = arrayComputador.map((item, index, array) =>{
+         return item.valor
+      })
+   
+      valorUsuarioTotal = valorUsuario[0] + valorUsuario[1]
+      valorComputadorTotal = valorComputador[0] + valorComputador[1]
+   console.log (`Usuario: Cartas: ${maoUsuario}. Pontuação: ${valorUsuarioTotal}.
+   Computador: Cartas: ${maoComputador}. Pontuação: ${valorComputadorTotal}`)
+   if(maoUsuario.texto === ["A", "A"]){confirm(
+      `Suas cartas são ${maoUsuario}, por as cartas serão sorteadas novamente` +
+      "\n"+ 
+      "Entendido?"
+   )}else if(maoComputador.texto === ["A", "A"]){confirm(
+      `As cartas do computador são ${maoComputador}, por isso as caras serão sorteadas novamente` +
+      "\n"+ 
+      "Entendido?"
+   )}
+
+
+
+    if((valorUsuarioTotal > valorComputadorTotal) <= 21) {
+       console.log ("O Usuario venceu!")}else if((valorUsuarioTotal < valorComputador) <= 21){
+       console.log ("O computador venceu!")
+       }else if(valorUsuarioTotal = valorComputadorTotal){
+           console.log ("Empate!!!")
+         }
+    } else {console.log ("O jogo acabou.")
+       
+    }
