9ac283b075f917091572943388860a3a3b4494d8
Rodolfo Dornelas <rodolfo.dpo@gmail.com>
2021-08-09 18:25:45 -0300

    exercicio finalizado

--- Semana9/aula31/insta-lab-hooks/src/components/Post/Post.js
+++ Semana9/aula31/insta-lab-hooks/src/components/Post/Post.js
@@ -17,14 +17,51 @@ const Post = (props) => {
   const [comentarios, setComentarios] = useState([])
 
   const onClickCurtida = () => {
+// Como se trata de uma função, e não mais de um método, é necessário 
+// mudar a sintaxe de método para função. 
+// Ou seja, basicamente, colocar uma const antes do onClickCurtida.
+// Isso já foi feito pra você no código do componente funcional.
+// Primeiro, no if, no lugar de this.state.curtido, deve vir apenas curtido, 
+// ou o nome da variável que guarda o estado na declaração do useState
+    if (curtido){
+
+	 // No lugar do this.setState, devem ser chamadas as respectivas funções
+	 // que mudam o valor de suas propriedades do estado. Ou seja, aqui, 
+	 // chamariamos a função setCurtido() e setNumeroCurtidas(). E como parâmetro
+	 // delas, passariamos os valores que queremos mudar:
+
+	    setCurtido(!curtido)
+	    setNumeroCurtidas(numeroCurtidas - 1)
+    }else {
+      	  // o mesmo dito no último comentário, vale aqui também. No lugar de um 
+		// this.setState, chamariamos as funções que mudam seus respectivos 
+		// valores do estado, declaradas no passo 7, com seus respectivos parâmetros: 
+
+		  setCurtido(!curtido)
+	    setNumeroCurtidas(numeroCurtidas + 1)
+    }
   };
 
   const onClickComentario = () => {
+    setComentando(!comentando)
   };
 
   const enviarComentario = (comentario) => {
+    const listaDeComentarios = [...comentarios, comentario]
+    setComentarios(listaDeComentarios)
+    setComentando(false)
+    setNumeroComentarios(numeroComentarios + 1)
   }
-
+  const iconeCurtida = curtido ? (iconeCoracaoPreto) : (iconeCoracaoBranco);
+  const caixaDeComentario = comentando ? (
+  <SecaoComentario enviarComentario={enviarComentario}/>
+  ) : (comentarios.map(comentario => {
+    return (
+      <CommentContainer>
+        <p>{comentario}</p>
+      </CommentContainer>
+    )
+  }))
   return (
     <PostContainer>
       <PostHeader>
@@ -36,18 +73,18 @@ const Post = (props) => {
 
       <PostFooter>
         <IconeComContador
-          // icone={iconeCurtida}
+          icone={iconeCurtida}
           onClickIcone={onClickCurtida}
-          // valorContador={numeroCurtidas}
+          valorContador={numeroCurtidas}
         />
 
         <IconeComContador
           icone={iconeComentario}
           onClickIcone={onClickComentario}
-          // valorContador={numeroComentarios}
+          valorContador={numeroComentarios}
         />
       </PostFooter>
-      {/* {caixaDeComentario} */}
+      {caixaDeComentario}
     </PostContainer>
   )
 }
